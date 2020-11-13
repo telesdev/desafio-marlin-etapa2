2d3cef47a2797a73e71f7067014d527b45061fbd
Thyago Teles <thyago.ferreira@al.infnet.edu.br>
2020-11-13 16:39:30 -0300

    iniciando criaçao de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './views/home/home.component';
+import { PostListComponent } from './views/home/post-list/post-list.component';
+import { HeaderComponent } from './views/home/header/header.component';
+import { AsideComponent } from './views/home/aside/aside.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostListComponent,
+    HeaderComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule,
